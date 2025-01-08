64fc0f902e3ad230fc6efd3479116b92cd077334
KrishN-hub <krishnakumarpirunthavan@gmail.com>
2025-01-08 18:53:42 +0530

    getx localization change the languge of  the app

---
+++ lib/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetX tutorials'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ListTile(
+            title: Text('message'.tr),
+            subtitle: Text('name'.tr),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            children: [
+              OutlinedButton(
+                  onPressed: () {
+                    Get.updateLocale(Locale('en', 'US'));
+                  },
+                  child: Text('English')),
+              SizedBox(
+                width: 20,
+              ),
+              
+              OutlinedButton(
+                  onPressed: () {
+                    Get.updateLocale(Locale('ta', 'SL'));
+                  },
+                  child: Text('Tamil'))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/languages.dart
@@ -0,0 +1,9 @@
+import 'package:get/get_navigation/src/root/internacionalization.dart';
+
+class Languages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': {'message': 'what is your name?', 'name': 'pirunthavan'},
+        'ta_SL': {'message': 'உன் பெயர் என்ன?', 'name': 'பிருந்தவன்'},
+      };
+}
