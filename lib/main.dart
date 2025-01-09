e20fdfff06d5a4d9e15a9bfa77980d1062d00488
KrishN-hub <krishnakumarpirunthavan@gmail.com>
2025-01-09 10:03:19 +0530

    dark theme makes work

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const HomeScreen());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData.dark(), // Define a dark theme
+      themeMode: ThemeMode.light, // Default theme mode
+      home: const HomeScreen(),
+    );
   }
 }
