fa67dcf6a12090debf6cb92d6b5ebfc0729b1b18
KrishN-hub <krishnakumarpirunthavan@gmail.com>
2025-01-16 12:15:59 +0530

    Utils (toastmessage, snackbar)

---
+++ lib/res/colors/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color blackColor = Color(0xff000000);
+  static const Color whitwColor = Color(0xffffffff);
+}
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:master_class/utils/utils.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +11,13 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Utils.toastMessageCenter('KrishN');
+          Utils.toastMessage('krishnakumar');
+        },
+      ),
+    );
   }
 }
